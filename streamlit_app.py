895e5c5e02bd45f798f164d7a2e3201cc7ae2656
jorrgett <jorgerafaeltorrestoro@gmail.com>
2023-04-03 00:11:32 -0400

    tumor glioma

--- streamlit_app.py
+++ streamlit_app.py
@@ -50,13 +50,23 @@ def predict(image):
     if classification == 0:
         return "No se detectó ningún tumor en la imagen. ¡Felicidades! Según nosotros, no tiene un tumor cerebral, pero le recomendamos encarecidamente que visite a un médico para asegurarse de que está realmente a salvo. En nuestra base de datos contamos con algunos médicos que le podrán verificar la información."
     elif classification == 1:
-        st.write("Se detectó un tumor pituitario en la imagen.")
-        st.write("Haga clic en el siguiente enlace para obtener más información:")
-        st.markdown("<a href='https://www.cancer.gov/espanol/tipos/tumor-de-hipofisis' target='_blank'>Más información sobre Tumor Pituitario</a>", unsafe_allow_html=True)
+        if st.button('Más información sobre Tumor Pituitario'):
+            webbrowser.open('https://www.cancer.gov/espanol/tipos/tumor-de-hipofisis')
+        return "Se detectó un tumor pituitario en la imagen. Los tumores pituitarios son crecimientos anormales que se desarrollan en la glándula pituitaria. Algunos tumores pituitarios dan como resultado demasiadas hormonas que regulan funciones importantes de su cuerpo. Algunos tumores pituitarios pueden hacer que la glándula pituitaria produzca niveles más bajos de hormonas. Aun así le recomendamos encarecidamente que contacte a un médico para asegurarse de que realmente la información sea verídica. En nuestra base de datos contamos con algunos médicos que le podrán verificar la información."
     elif classification == 2:
         return "Se detectó un tumor meningioma en la imagen. Un meningioma es un tumor que surge de las meninges, las membranas que rodean el cerebro y la médula espinal. Aunque técnicamente no es un tumor cerebral, se incluye en esta categoría porque puede comprimir o apretar el cerebro, los nervios y los vasos adyacentes. Aun así le recomendamos encarecidamente que contacte a un médico para asegurarse de que realmente la información sea verídica. En nuestra base de datos contamos con algunos médicos que le podrán verificar la información."
     elif classification == 3:
-        return "Se detectó un tumor glioma en la imagen. El glioma es un tipo de tumor que se presenta en el cerebro y la médula espinal. Los gliomas comienzan en las células de apoyo pegajosas (células gliales) que rodean las células nerviosas y las ayudan a funcionar. Aun así le recomendamos encarecidamente que contacte a un médico para asegurarse de que realmente la información sea verídica. En nuestra base de datos contamos con algunos médicos que le podrán verificar la información."
+        st.write("Se detectó un tumor glioma en la imagen.")
+        st.write("El glioma es un tipo de tumor que se presenta en el cerebro y la médula espinal. Los gliomas comienzan en las células de apoyo pegajosas (células gliales) que rodean las células nerviosas y las ayudan a funcionar.")
+        st.write("Tres tipos de células gliales pueden producir tumores. Los gliomas se clasifican según el tipo de célula glial involucrada en el tumor, así como las características genéticas del tumor, lo que puede ayudar a predecir cómo se comportará el tumor con el tiempo y los tratamientos que tienen más probabilidades de funcionar.")
+        st.write("Los tipos de glioma incluyen:")
+        st.write("Astrocitomas, incluidos astrocitoma, astrocitoma anaplásico y glioblastoma.")
+        st.write("Ependimomas, incluidos el ependimoma anaplásico, el ependimoma mixopapilar y el subependimoma.")
+        st.write("Oligodendrogliomas, incluidos oligodendroglioma, oligodendroglioma anaplásico y oligoastrocitoma anaplásico.")
+        st.write("Un glioma puede afectar su función cerebral y poner en peligro la vida según su ubicación y la tasa de crecimiento. Los gliomas son uno de los tipos más comunes de tumores cerebrales primarios.")
+        st.write("El tipo de glioma que tiene ayuda a determinar su tratamiento y su pronóstico. En general, las opciones de tratamiento del glioma incluyen cirugía, radioterapia, quimioterapia, terapia dirigida y ensayos clínicos experimentales.")
+        st.write("Aun así le recomendamos encarecidamente que contacte a un médico para asegurarse de que realmente la información sea verídica. En nuestra base de datos contamos con algunos médicos que le podrán verificar la información. A continuación le proporcionamos el link para que pueda enviar sus datos.")
+        st.markdown("<a href='https://main-brainly.vercel.app/profile' target='_blank'>Envíar Datos</a>", unsafe_allow_html=True)
 
 def main():
     st.title("Detección de tumores cerebrales")
